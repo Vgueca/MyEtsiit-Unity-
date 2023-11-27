7cccd8b441bfce1ca5b18f56d09cabbe5aa9a05f
Valentín Guerrero Cano <70842575+Vgueca@users.noreply.github.com>
2023-11-27 10:30:51 +0100

    Testing
    Distintas escenas para testear unity y el ultraleap. Ahora mismo la escena Testing es la principal.

---
+++ Assets/Scripts/MainBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //disable gameobject
+    public void disable(){
+        Debug.Log("Iniciando");
+        this.gameObject.SetActive(false);
+    }
+}
