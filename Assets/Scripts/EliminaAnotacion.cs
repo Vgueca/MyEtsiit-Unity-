c9dd0bfcee4813c6443e2b5467ad9b9ca34d5490
nasrfarissi <nasrfarissi@gmail.com>
2023-11-28 18:19:01 +0100

    interfaz bonita total

---
+++ Assets/Scripts/EliminaAnotacion.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetButtonAnimation : MonoBehaviour
+{
+    private Button button;
+    private Animator animator;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        animator.Play("Normal");
+    }
+}
